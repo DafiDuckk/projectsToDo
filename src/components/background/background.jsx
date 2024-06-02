f7f42c597d9101b15222afa704579f76d671a1a0
Nikita <burundukxx@gmail.com>
2024-06-02 14:22:38 -0800

    setting

---
+++ src/components/background/background.jsx
@@ -0,0 +1,11 @@
+import './background.css' 
+function background() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default background
+
---
+++ src/components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+import './navbar.css'
+
+function navbar() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default navbar
